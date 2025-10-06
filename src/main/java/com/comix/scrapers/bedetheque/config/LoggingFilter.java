89fd082c5001fb686f1a20019e1fae1ff78e4de7
twenty-cents <vincent.roger.dev@gmail.com>
2025-10-06 21:22:06 +0200

    updated cfg.

--- src/main/java/com/comix/scrapers/bedetheque/config/LoggingFilter.java
+++ src/main/java/com/comix/scrapers/bedetheque/config/LoggingFilter.java
@@ -3,6 +3,7 @@ package com.comix.scrapers.bedetheque.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 @Component
 @Order(1)
+@Slf4j
 public class LoggingFilter implements Filter {
 
     private static final String TRACEID_HEADER = "X-B3-TraceId";
@@ -20,6 +22,7 @@ public class LoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String traceId;
+        log.info("Incoming request: {} {}", ((HttpServletRequest) request).getMethod(), ((HttpServletRequest) request).getRequestURI());
         try {
             MDC.put("userIP", this.getClientIp((HttpServletRequest) request));
             MDC.put("user-agent", this.getUserAgent((HttpServletRequest) request));
