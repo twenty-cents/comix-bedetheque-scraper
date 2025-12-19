917c7307362fcdb0b983db6829ef2714a2a043e7
twenty-cents <vincent.roger.dev@gmail.com>
2025-12-19 09:46:57 +0100

    Restricted permissions on nfs media files.

--- src/main/java/com/comix/scrapers/bedetheque/client/scraper/GenericScraper.java
+++ src/main/java/com/comix/scrapers/bedetheque/client/scraper/GenericScraper.java
@@ -87,14 +87,10 @@ public class GenericScraper extends Scraper {
             // user permission
             perms.add(PosixFilePermission.OWNER_READ);
             perms.add(PosixFilePermission.OWNER_WRITE);
-            perms.add(PosixFilePermission.OWNER_EXECUTE);
             // group permissions
             perms.add(PosixFilePermission.GROUP_READ);
-            perms.add(PosixFilePermission.GROUP_EXECUTE);
             // others permissions removed
             perms.remove(PosixFilePermission.OTHERS_READ); // Compliant
-            perms.remove(PosixFilePermission.OTHERS_WRITE); // Compliant
-            perms.remove(PosixFilePermission.OTHERS_EXECUTE); // Compliant
             Files.setPosixFilePermissions(f.toPath(), perms);
             log.debug("Permissions 777 set on file: {}", mediaFilenamePath);
         } catch (UnsupportedOperationException | IOException e) {
